281a2e58c7fe4e4eb028e61d561378722535a91f
vpugachev <vpugachev@uma.tech>
2020-02-10 16:23:08 +0300

    add iteratot

--- list/list.py
+++ list/list.py
@@ -13,32 +13,56 @@ class NodeItem:
         self.next = next
 
 
+class ListIterator:
+    def __init__(self, source_list):
+        self.current_head = source_list.get_head()
+
+    def __next__(self):
+        if self.current_head:
+            value = self.current_head.get()
+            self.current_head = self.current_head.get_next()
+            return value
+        else:
+            raise StopIteration
+
+
 class List:
     def __init__(self, *args):
-        self.head = None
+        self.__head = None
         for value in args:
             self.__add(value)
 
-    def __add(self, value):
-        temp = NodeItem(value)
-        temp.set_next(self.head)
-        self.head = temp
+    def __iter__(self):
+        return ListIterator(self)
 
     def __str__(self):
-        current_head = self.head
         result = ''
-        while current_head:
-            result = str(current_head.get()) + result
-            current_head = current_head.get_next()
-            if current_head:
-                result = ',' + result
+        for item in self:
+            result = str(item) + result
         return result
+
+    def __add(self, value):
+        temp = NodeItem(value)
+        temp.set_next(self.__head)
+        self.__head = temp
+
+    def get_head(self):
+        return self.__head
+
+    def append(self, *args):
+        for value in args:
+            self.__add(value)
+
+    def print(self):
+        print(self)
 
 
 def main():
     a = List(1, 2, 3)
-    print(a)
-    print('hello')
+    a.print()
+
+    # a.append(4, 5)
+    # a.print()
 
 
 if __name__ == "__main__":
